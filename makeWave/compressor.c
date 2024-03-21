f7f957860c6c4154dd566642a28741b3ba80e9ff
inoue <xxxsunshine9981@gmail.com>
2024-03-21 09:19:19 +0900

    makeWave; compressor で attack_time release_timeの実装

--- makeWave/compressor.c
+++ makeWave/compressor.c
@@ -5,39 +5,52 @@
 #include "../include/file.h"
 
 /* 
-this program apply compressor effect to wav files
+	this program apply compressor effect to wav files
+	usage : program (input file) (output file) (threshold) (ratio) (attack time) (release time) (time_frame)
+	threshold : 0 ~ 1
+	ratio : 0 ~ 1
+	attack : (sec)
+	release : (sec)
+	time_frame : how long a window is (sec)
 */
 
 int main(int argc, char *argv[]) {
-	FILE *fp;
-	float time_frame = 0.010; //ms
+	FILE *fp_read, *fp_write;
 
+	/* initialization */
 	WAVE_FORMAT wave_format = {0};
-	PCM pcm;
-	COMPRESSOR comp = {0.0, 0.0, 0.0, 0.0};
-	comp.threshold = 0.110;
-	comp.ratio = 0.500;
+	SOUND_DATA sound_data = {0};
+	COMPRESSOR comp = {0};
+	comp.threshold = atof(argv[3]);
+	comp.ratio = atof(argv[4]);
+	comp.attack = atof(argv[5]);
+	comp.release = atof(argv[6]);
+	comp.timeFrame = atof(argv[7]);
 
-	fp = fopen(argv[1], "rb");
-	if(!fp) {
-		printf("file open error\n");
+	fp_read = fopen(argv[1], "rb");
+	if(!fp_read) {
+		printf("file open error : %s\n", argv[1]);
 		exit(0);
 	}
 
-	// read Chunks
-	wave_readRiffChunk(fp, &wave_format);
-	wave_readFmtChunk(fp, &wave_format);
-	wave_readDataChunk(fp, &wave_format, &pcm);
+	/* read Chunks */
+	wave_readRiffChunk(fp_read, &wave_format.riffChunk);
+	wave_readFmtChunk(fp_read, &wave_format.fmtChunk);
+	wave_readDataChunk(fp_read, &wave_format, &sound_data);
+	fclose(fp_read);
 
-	comp.windowSize = (int) (wave_format->fmtChunk.sampleRate * wave_format-fmtChunk.numChannels * time_frame);
+	fp_write = fopen(argv[2], "wb");
+	if(!fp_write) {
+		printf("file open error : %s\n", argv[2]);
+		exit(0);
+	}
 
-	// apply compressor effect
-	wave_rmsComp(&pcm, time_frame, &comp);
-	// write wav file
-	wave_writeWavFile(&wave_format, &pcm, "compressor.wav");
+	/* write data */
+	wave_rmsComp(&wave_format, &comp, &sound_data); // apply compressor effect
+	wave_writeWavFile(fp_write, &wave_format, &sound_data);
 
-	free(pcm.s);
-	fclose(fp);
+	free(sound_data.s);
+	fclose(fp_write);
 
 	return 0;
 }
